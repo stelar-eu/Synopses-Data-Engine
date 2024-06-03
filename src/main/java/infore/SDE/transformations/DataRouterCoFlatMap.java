252044886dc7c4503fae37a0c894b870e15cdf59
Dimitris Petrou <petroudimitris@outlook.com>
2024-06-03 15:23:46 +0300

    Comments added at the Operator, more pending

--- src/main/java/infore/SDE/transformations/DataRouterCoFlatMap.java
+++ src/main/java/infore/SDE/transformations/DataRouterCoFlatMap.java
@@ -15,18 +15,29 @@ import org.apache.flink.util.Collector;
 
 import java.util.*;
 
-public class dataRouterCoFlatMap extends RichCoFlatMapFunction<Datapoint, Request, Datapoint> {
+public class DataRouterCoFlatMap extends RichCoFlatMapFunction<Datapoint, Request, Datapoint> {
 
     private static final long serialVersionUID = 1L;
     // SourceID (1-5), StreamID(1-10000), Keys(1-1000),
 
+
+
     //UID, parallelism, index,
     //Parallelism //Number, index
+    /**
+     * A map that holds the parallelism configuration for keyed streams,
+     * mapping the number of parallel instances to a tuple containing
+     * the count and index.
+     */
     private HashMap<Integer, Tuple2<Integer, Integer>> KeyedParallelism = new HashMap<>();
+
+
     //Parallelism //Number, index
     private HashMap<Integer, Tuple2<Integer, Integer>> RandomParallelism = new HashMap<>();
-    //StreamID                 //Parallelism, KEYs
+    //StreamID //Parallelism, KEYs
     private HashMap<String, ArrayList<Tuple2<Integer, String>>> KeysPerStream = new HashMap<>();
+
+
     private HashMap<String, Request> Synopses = new HashMap<>();
 
     private HashMap<String, RadiusSketch> MapToGrid = new HashMap<>();
@@ -35,29 +46,32 @@ public class dataRouterCoFlatMap extends RichCoFlatMapFunction<Datapoint, Reques
     private transient ValueState<Tuple1<String>> rs;
     private int pId;
 
+    /**
+     * This flatMap1 method handles the arrival of DataPoint objects in our stream
+     * @param dataNode The stream element in this case is a DataPoint object
+     * @param out The collector to emit resulting elements to, in this case we emit modified DataPoints
+     * @throws Exception
+     */
     @Override
-    public void flatMap1(Datapoint value, Collector<Datapoint> out) throws Exception {
+    public void flatMap1(Datapoint dataNode, Collector<Datapoint> out) throws Exception {
 
-        if (MapToGrid.containsKey(value.getDataSetkey())) {
-            RadiusSketch RS = MapToGrid.get(value.getDataSetkey());
-            ArrayList<Datapoint> sketches_to_grid = (ArrayList<Datapoint>) RS.estimate(value.getValues());
+        // Handle the case of RadiusSketch
+        if (MapToGrid.containsKey(dataNode.getDataSetkey())) {
+            RadiusSketch RS = MapToGrid.get(dataNode.getDataSetkey());
+            ArrayList<Datapoint> sketches_to_grid = (ArrayList<Datapoint>) RS.estimate(dataNode.getValues());
             for (Datapoint dp : sketches_to_grid) {
                 out.collect(dp);
             }
         }
 
-        //Send Data with default Key the StreamID
-        // value.f0 = value_tokens[0];
-        //out.collect(value);
-        //System.out.println(pId+" size "+ KeyedParallelism.size());
         if (KeyedParallelism.size() > 0) {
-            ArrayList<Tuple2<Integer, String>> tmp = KeysPerStream.get(value.getStreamID());
+            ArrayList<Tuple2<Integer, String>> tmp = KeysPerStream.get(dataNode.getStreamID());
             if (tmp == null) {
                 tmp = new ArrayList<>();
                 for (Map.Entry<Integer, Tuple2<Integer, Integer>> entry : KeyedParallelism.entrySet()) {
                     Integer key = entry.getKey();
                     Tuple2<Integer, Integer> v = entry.getValue();
-                    tmp.add(new Tuple2<>(key, value.getDataSetkey() + "_" + key + "_KEYED_" + v.f1));
+                    tmp.add(new Tuple2<>(key, dataNode.getDataSetkey() + "_" + key + "_KEYED_" + v.f1));
                     v.f1++;
                     if (v.f1 >= key) {
                         //v.f1 = 0;
@@ -65,15 +79,12 @@ public class dataRouterCoFlatMap extends RichCoFlatMapFunction<Datapoint, Reques
                         entry.setValue(new Tuple2<>(key, 0));
                     }
                 }
-                KeysPerStream.put(value.getStreamID(), tmp);
+                KeysPerStream.put(dataNode.getStreamID(), tmp);
             }
             for (Tuple2<Integer, String> t : tmp) {
-                value.setDataSetkey(t.f1);
-                out.collect(value);
+                dataNode.setDataSetkey(t.f1);
+                out.collect(dataNode);
             }
-
-        }
-        if (RandomParallelism.size() > 0) {
 
         }
         if (RandomParallelism.size() > 0) {
@@ -81,9 +92,9 @@ public class dataRouterCoFlatMap extends RichCoFlatMapFunction<Datapoint, Reques
 
                 Integer key = entry.getKey();
                 Tuple2<Integer, Integer> v = entry.getValue();
-                value.setDataSetkey(value.getDataSetkey() + "_" + key + "_RANDOM_" + v.f1);
+                dataNode.setDataSetkey(dataNode.getDataSetkey() + "_" + key + "_RANDOM_" + v.f1);
 
-                out.collect(value);
+                out.collect(dataNode);
                 v.f1++;
                 if (v.f1 == key) {
                     v.f1 = 0;
@@ -93,72 +104,72 @@ public class dataRouterCoFlatMap extends RichCoFlatMapFunction<Datapoint, Reques
         }
     }
 
+    /**
+     * This flatMap2 method handles the arrival of Request objects in the stream
+     * and handles the parallelism.
+     * @param request The stream element in this case is a Request object
+     * @param out The collector to emit resulting elements to, nothing is emitted in this case
+     * @throws Exception
+     */
     @Override
-    public void flatMap2(Request rq, Collector<Datapoint> out) throws Exception {
+    public void flatMap2(Request request, Collector<Datapoint> out) throws Exception {
 
-        if (rq.getSynopsisID() == 100) {
-            MapToGrid.put(rq.getDataSetkey(), new RadiusSketch(rq.getUID(), rq.getParam()[0], rq.getParam()[1], "Partitioner", rq.getDataSetkey(), rq.getParam()));
+        if (request.getSynopsisID() == 100) {
+            MapToGrid.put(request.getDataSetkey(), new RadiusSketch(request.getUID(), request.getParam()[0], request.getParam()[1], "Partitioner", request.getDataSetkey(), request.getParam()));
             return;
         }
 
-        if (rq.getRequestID() == 5)
-            rq.setRequestID(1);
 
-        if (rq.getRequestID() == 1 || rq.getRequestID() == 4) {
+        // The handling for Add requests (Regular/Continuous Add) follows the same logic for this operator
+        if (request.getRequestID() == 5)
+            request.setRequestID(1);
 
-            if (rq.getNoOfP() > 1) {
+        // When receiving an Add request in case of parallelism greater than 1
+        // generate new dataset keys ready upon which the incoming data tuples will be routed
+        if (request.getRequestID() == 1 || request.getRequestID() == 4) {
 
-                if (rq.getRequestID() == 1) {
-                    if (KeyedParallelism.get(rq.getNoOfP()) == null) {
-                        KeyedParallelism.put(rq.getNoOfP(), new Tuple2<>(1, 0));
+            if (request.getNoOfP() > 1) {
+
+                // Add synopsis with Keyed Partitioning (either Regular or Continuous)
+                // The following code snippet generates the keys for the corresponding
+                // partitions based on the desired number of parallelism
+                if (request.getRequestID() == 1) {
+
+                    // Checks if no partition keys are currently generated
+                    if (KeyedParallelism.get(request.getNoOfP()) == null) {
+                        KeyedParallelism.put(request.getNoOfP(), new Tuple2<>(1, 0));
                         int i = 0;
                         for (ArrayList<Tuple2<Integer, String>> v : KeysPerStream.values()) {
-                            v.add(new Tuple2<>(rq.getNoOfP(), rq.getKey() + "_" + rq.getNoOfP() + "_KEYED_" + i));
+                            v.add(new Tuple2<>(request.getNoOfP(), request.getKey() + "_" + request.getNoOfP() + "_KEYED_" + i));
                             i++;
-                            if (i == rq.getNoOfP())
+                            if (i == request.getNoOfP())
                                 i = 0;
                         }
                     } else {
-                        Tuple2<Integer, Integer> t = KeyedParallelism.get(rq.getNoOfP());
+                        Tuple2<Integer, Integer> t = KeyedParallelism.get(request.getNoOfP());
                         t.f0 += 1;
-                        //System.out.println("HERE_> " + t.f0);
-                        KeyedParallelism.put(rq.getNoOfP(), t);
+                        KeyedParallelism.put(request.getNoOfP(), t);
                     }
-                } else if (rq.getRequestID() == 4) {
-                    if (RandomParallelism.get(rq.getNoOfP()) == null) {
-                        KeyedParallelism.put(rq.getNoOfP(), new Tuple2<>(1, 0));
+                } else if (request.getRequestID() == 4) {
+
+                    if (RandomParallelism.get(request.getNoOfP()) == null) {
+                        //????? IS THIS CORRECT? SHOULDN'T IT BE RandomParallelism HUH?
+                        KeyedParallelism.put(request.getNoOfP(), new Tuple2<>(1, 0));
                     } else {
-                        Tuple2<Integer, Integer> t = RandomParallelism.get(rq.getNoOfP());
+                        Tuple2<Integer, Integer> t = RandomParallelism.get(request.getNoOfP());
                         t.f0++;
-                        RandomParallelism.put(rq.getNoOfP(), t);
+                        RandomParallelism.put(request.getNoOfP(), t);
                     }
 
 
                 }
 
             }
-            String newS = rq.toSumString();
-/*
-            if (rs.value() == null) {
+        // When receiving a Remove request
+        // generate new dataset keys ready upon which the incoming data tuples will be routed
+        } else if (request.getRequestID() == 2) {
 
-                Tuple1<String> tmp = new Tuple1<String>(newS);
-                Synopses.put(""+rq.getUID(), rq);
-                rs.update(tmp);
-
-                //System.out.print("\n StateInit");
-            } else {
-
-                Synopses.put("" +rq.getUID(), rq);
-                Tuple1<String> tmp = rs.value();
-                tmp.f0 = newS.concat(tmp.f0);
-                //System.out.print("\n StateUpdate");
-                rs.update(tmp);
-            }
-             */
-
-        } else if (rq.getRequestID() == 2) {
-
-            Request re = Synopses.remove("" + rq.getUID());
+            Request re = Synopses.remove("" + request.getUID());
             if (re != null) {
 
                 if (re.getRequestID() == 1) {
@@ -198,6 +209,7 @@ public class dataRouterCoFlatMap extends RichCoFlatMapFunction<Datapoint, Reques
                     Tuple1<String> tmp2 = new Tuple1<>(tmp);
                     // rs.update(tmp2);
 
+
                 } else {
                    // rs.update(new Tuple1<String>(""));
                 }
@@ -207,9 +219,7 @@ public class dataRouterCoFlatMap extends RichCoFlatMapFunction<Datapoint, Reques
 
     public void open(Configuration config) {
 
-
-        TypeInformation<Tuple1<String>> typeInformation = TypeInformation.of(new TypeHint<Tuple1<String>>() {
-        });
+        TypeInformation<Tuple1<String>> typeInformation = TypeInformation.of(new TypeHint<Tuple1<String>>() {});
 
         ValueStateDescriptor descriptor = new ValueStateDescriptor("Synopses", typeInformation);
         descriptor.setQueryable("getSynopses");
