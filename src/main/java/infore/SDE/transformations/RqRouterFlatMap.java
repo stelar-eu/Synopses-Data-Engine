5a0f5ca52adc3ee487157ec77e50da810c6f641c
Dimitris Petrou <petroudimitris@outlook.com>
2024-06-03 15:30:33 +0300

    Beautified RqRouterFlatMap.java

--- src/main/java/infore/SDE/transformations/RqRouterFlatMap.java
+++ src/main/java/infore/SDE/transformations/RqRouterFlatMap.java
@@ -1,73 +1,67 @@
 package infore.SDE.transformations;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
 import org.apache.flink.util.Collector;
-
 import infore.SDE.messages.Request;
 
-public class RqRouterFlatMap extends RichFlatMapFunction<Request, Request> implements Serializable{
+public class RqRouterFlatMap extends RichFlatMapFunction<Request, Request> implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-   //SourceID + Uid (1-1000),Keys(1-1000),        
 	@Override
 	public void flatMap(Request rq, Collector<Request> out) throws Exception {
+		if (rq.getRequestID() % 10 >= 8) {
+			return;
+		}
 
-			 //ADD-REMOVE-ESTIMATE SKETCH FOR A STREAM
-			  if( rq.getRequestID()%10 < 8) {
-				  if(rq.getNoOfP() == 1)
-				  out.collect(rq);
-				  else {
-					  String tmpkey = rq.getKey();
-					  if(rq.getRequestID()%10 == 6) {
-							int n = Integer.parseInt(rq.getParam()[0]);
-							String[] dataSets = rq.getDataSetkey().split(",");
-							String[] tpr = rq.getParam();
-							String[] pr = rq.getParam();
-							pr[1]=""+rq.getUID();
+		String tmpkey = rq.getKey();
 
-							for(int i=0;i< n;i++){
+		if (rq.getNoOfP() == 1) {
+			out.collect(rq);
+			return;
+		}
 
-								tmpkey = dataSets[i];
-								rq.setUID(Integer.parseInt(tpr[i+1]));
+		if (rq.getRequestID() % 10 == 6) {
+			handleRequestID6(rq, tmpkey, out);
+		} else if (rq.getSynopsisID() == 100) {
+			handleSynopsisID100(rq, tmpkey, out);
+		} else {
+			handleDefaultCase(rq, tmpkey, out);
+		}
+	}
 
-								for(int j = 0; j < rq.getNoOfP(); j ++) {
+	private void handleRequestID6(Request rq, String tmpkey, Collector<Request> out) {
+		int n = Integer.parseInt(rq.getParam()[0]);
+		String[] dataSets = rq.getDataSetkey().split(",");
+		String[] params = rq.getParam();
+		params[1] = String.valueOf(rq.getUID());
 
-									rq.setKey(tmpkey + "_" + rq.getNoOfP() + "_KEYED_" + j);
-									out.collect(rq);
+		for (int i = 0; i < n; i++) {
+			rq.setUID(Integer.parseInt(params[i + 1]));
+			for (int j = 0; j < rq.getNoOfP(); j++) {
+				rq.setKey(dataSets[i] + "_" + rq.getNoOfP() + "_KEYED_" + j);
+				out.collect(rq);
+			}
+		}
+	}
 
-								}
+	private void handleSynopsisID100(Request rq, String tmpkey, Collector<Request> out) {
+		int iterations = Integer.parseInt(rq.getParam()[2]);
+		for (int i = 0; i < iterations; i++) {
+			rq.setDataSetkey(tmpkey + "_" + i);
+			out.collect(rq);
+		}
+	}
 
-							}
-					  }else{
-						  if(rq.getSynopsisID() == 100){
-							  for(int i=0;i<Integer.parseInt(rq.getParam()[2]);i++){
-								  rq.setDataSetkey(tmpkey + "_" + i);
-
-								  out.collect(rq);
-
-							  }
-							  return;
-						  }
-						  for(int i = 0; i < rq.getNoOfP(); i ++) {
-
-						  	if(rq.getRequestID()%10 == 4){
-								rq.setKey(tmpkey +"_"+rq.getNoOfP()+"_RANDOM_" + i);
-								out.collect(rq);
-							}else{
-						  		//System.out.println("here ->" + i );
-								rq.setDataSetkey(tmpkey + "_" + rq.getNoOfP() + "_KEYED_" + i);
-
-								out.collect(rq);
-								//rq.setKey(tmpkey +"_"+rq.getNoOfP()+"_RANDOM_" + i);
-								//out.collect(rq);
-							}
-						  }
-					  }
-				  }
-			  }	  
+	private void handleDefaultCase(Request rq, String tmpkey, Collector<Request> out) {
+		for (int i = 0; i < rq.getNoOfP(); i++) {
+			if (rq.getRequestID() % 10 == 4) {
+				rq.setKey(tmpkey + "_" + rq.getNoOfP() + "_RANDOM_" + i);
+			} else {
+				rq.setDataSetkey(tmpkey + "_" + rq.getNoOfP() + "_KEYED_" + i);
+			}
+			out.collect(rq);
+		}
 	}
 }
